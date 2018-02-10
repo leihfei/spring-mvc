f39d17bf27810f07e05f1f345169a7e9e28bacac
雷洪飞 <leihfei@163.com>
2018-02-10 11:23:30 +0800

    基本mvc测试

---
+++ src/main/java/com/lnlr/mvc/TestMvc.java
@@ -0,0 +1,11 @@
+package com.lnlr.mvc;
+/**
+ * @author 雷洪飞 on 9:25 2018/2/10.
+ * desc
+ */
+public class TestMvc {
+
+    public TestMvc() {
+        System.out.println("创建无参数构造函数....");
+    }
+}
---
+++ src/main/java/com/lnlr/mvc/TestMvcDispatcher.java
@@ -0,0 +1,11 @@
+package com.lnlr.mvc;
+/**
+ * @author 雷洪飞 on 9:25 2018/2/10.
+ * desc
+ */
+public class TestMvcDispatcher {
+
+    public TestMvcDispatcher() {
+        System.out.println("创建Dispatcher无参数构造函数....");
+    }
+}
